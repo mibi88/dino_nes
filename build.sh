e9ec1ea2d2e53384aac0d10b14c10f1a4691f9ac
mibi88 <mbcontact50@gmail.com>
2024-05-25 16:10:03 +0200

    Linking to NROM 128

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 echo "=====> Building 'src/main.asm' with ca65 ... <====="
 ca65 ./src/main.asm -o ./bin/main.o -W 2 -t nes
 od65 -S ./bin/main.o
-ld65 ./bin/main.o -o ./bin/dino.nes -t nes
+ld65 ./bin/main.o -o ./bin/dino.nes -C nrom.cfg
 rm ./bin/*.o
 echo "=====> Done ! <====="
