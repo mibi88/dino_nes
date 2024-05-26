74516291ad137e4c69026a6d7cc2a94d4d0aa811
mibi88 <mbcontact50@gmail.com>
2024-05-26 15:29:37 +0200

    Removed the debug symbols

--- build_debug.sh
+++ build_debug.sh
@@ -3,6 +3,6 @@
 echo "=====> Building 'src/main.asm' with ca65 ... <====="
 ca65 ./src/main.asm -o ./bin/main.o -W 2 -t nes -g
 od65 -S ./bin/main.o
-ld65 ./bin/main.o -o ./bin/dino.nes -C nrom.cfg --dbgfile dino.dbg
+ld65 ./bin/main.o -o ./bin/dino.nes -C nrom.cfg --dbgfile ./bin/dino.dbg
 rm ./bin/*.o
 echo "=====> Done ! <====="
